0ca08116e761fc96346e583ea39a748a1682e25b
Henryotis <henryotis113@gmail.com>
2022-07-03 13:56:59 -0700

    final two

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
-
-	/*
-	* main - Entry point
-	*
+/**
+	*main - Prints 'with proper grammar, but the ouptu is a piece of art,'
 	* Description: this project prints with proper grammer
-	*
 	* Return: Always 0 (success)
 	*/
 int main(void)
