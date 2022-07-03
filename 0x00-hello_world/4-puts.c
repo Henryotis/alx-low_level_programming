5bdd25a6108a5190e789e0fb4e2ccb74c3f91e2a
Henryotis <henryotis113@gmail.com>
2022-07-03 13:23:05 -0700

    another try

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 /*
-	* function main is the entry point
-	* Description 'the project will describe the provided string'
-	*
+	* main - Entry point
+	* Description: 'the project will describe the provided string'
 	* Return: Always 0 (sucess)
 	*/
 int main(void)
-	/*
-	 * this is the entry point
-	 */
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
