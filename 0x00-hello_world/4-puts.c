7f85a27470a5c37ec868d974bc8f473fd5c3d58a
Henryotis <henryotis113@gmail.com>
2022-07-03 13:39:51 -0700

    4 might be done now

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-/*
-	* main - prints provided text
+/**
+	* main - Write 'Programming is like building a multilingual puzzle'
 	* Description: 'the project will describe the provided string'
 	* Return: Always 0 (success)
 	*/
