abb87cfcaf0c41afd66c52036d6edefd2ebf8a67
Henryotis <henryotis113@gmail.com>
2022-07-06 13:03:19 -0700

    1.

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,11 +2,10 @@
 
 
 /**
-
  * print_alphabet - entry point
-
+ * Desription: a function that prints the alphabet, in 
+ * lowercase, followed by a new line.
  * Return: 0 Success
-
  */
 
 void print_alphabet(void)
