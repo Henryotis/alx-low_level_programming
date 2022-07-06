9a79a56c81a233af134c573547feb405156e39c4
Henryotis <henryotis113@gmail.com>
2022-07-06 12:54:29 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,29 @@
+nclude "main.h"
+
+
+/**
+
+ * print_alphabet - entry point
+
+ * Return: 0 Success
+
+ */
+
+void print_alphabet(void)
+
+{
+
+	char x;
+
+
+	for (x = 'a'; x <= 'z'; x++)
+
+	{
+
+		_putchar(x);
+
+	}
+
+	_putchar('\n');
+
+}
