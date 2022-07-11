9cafe497b2ff6ef93705baa46c41c797b720c9e2
Henryotis <henryotis113@gmail.com>
2022-07-11 13:38:36 -0700

    wer

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,8 +2,7 @@
 
 /**
  * reset_to_98 - resets value it points to 98
- * Description: a function that takes a pointer to an int as
- * parameter and updates the value it points to to 98
+ * @n: pointer
  * Return: 0
  */
 
