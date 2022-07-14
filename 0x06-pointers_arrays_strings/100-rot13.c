8f12b42489e9be78cee7ca4aff77fa937abb5d38
Henryotis <henryotis113@gmail.com>
2022-07-14 12:53:37 -0700

    next3

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,28 +1,28 @@
 #include "main.h"
 
 /**
- * rot13 - encodes a string using rot13\
+ * rot13 - encodes a string using rot13
  * @s: input string.
  * Return: the pointer to dest.
  */
 
 char *rot13(char *s)
 {
-	int count = 0, i;
+	int a = 0, i;
 	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-	while (*(s + count) != '\0')
+	while (*(s + a) != '\0')
 	{
 		for (i = 0; i < 52; i++)
 		{
-			if (*(s + count) == alphabet[i])
+			if (*(s + a) == letters[i])
 			{
-				*(s + count) = rot13[i];
+				*(s + a) = rot13[i];
 				break;
 			}
 		}
-		count++;
+		a++;
 	}
 
 	return (s);
